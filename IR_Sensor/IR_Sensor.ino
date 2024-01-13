85213d75333e7cb1ba4207e17fcfd42858edfdb9
CITRON_001 <mrsonsare@gmail.com>
2024-01-14 02:49:32 +0530

    new ir file

---
+++ IR_Sensor/IR_Sensor.ino
@@ -0,0 +1,23 @@
+const int input = 3;
+const int led = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(input,INPUT);
+pinMode(led,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(digitalRead(input));
+ if (digitalRead(input) == 0 )
+ {
+  digitalWrite(led,HIGH);
+ }
+ if (digitalRead(input) == 1)
+ {
+  digitalWrite(led,LOW);
+ }
+}
